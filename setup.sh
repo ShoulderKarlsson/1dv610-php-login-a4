e5c75c0c31c2187773618cb4a355816c4df7cd51
shoulderkarlsson <ak223fy@student.lnu.se>
2016-10-19 13:27:34 +0200

    made echo silent

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
+
 echo "Creating cookies.json"
 touch db/cookies.json
-echo "[]" | tee db/cookies.json
-
+echo "[]" | tee db/cookies.json > /dev/null
 
 echo "Creating accounts.json"
 touch db/accounts.json
-echo "[]" | tee db/accounts.json
+echo "[]" | tee db/accounts.json > /dev/null
 
 echo "Done - Ready to run!"
